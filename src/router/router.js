35cde2306b7b19363a7cfecfc1b4ea0dd1cead6e
chenjiang <chenjiang@lvjinsuo.com>
2018-11-12 16:52:12 +0800

    add 404 page

--- src/router/router.js
+++ src/router/router.js
@@ -9,6 +9,7 @@ import Home from 'bundle-loader?lazy&name=home!pages/Home/Home';
 import Page1 from 'bundle-loader?lazy&name=page1!pages/Page1/Page1';
 import Counter from 'bundle-loader?lazy&name=counter!pages/Counter/Counter';
 import UserInfo from 'bundle-loader?lazy&name=userInfo!pages/UserInfo/UserInfo';
+import NotFound from 'bundle-loader?lazy&name=notfound!pages/NotFound/NotFound';
 
 const createComponent = component => {
   return (props) => {
@@ -23,12 +24,15 @@ const createComponent = component => {
 };
 
 const getRouter = () => (
-  <Switch>
-    <Route exact path='/' component={createComponent(Home)}/>
-    <Route exact path='/page1' component={createComponent(Page1)}/>
-    <Route exact path='/counter' component={createComponent(Counter)}/>
-    <Route exact path='/userinfo' component={createComponent(UserInfo)}/>
-  </Switch>
+  <div>
+    <Switch>
+      <Route exact path='/' component={createComponent(Home)}/>
+      <Route exact path='/page1' component={createComponent(Page1)}/>
+      <Route exact path='/counter' component={createComponent(Counter)}/>
+      <Route exact path='/userinfo' component={createComponent(UserInfo)}/>
+      <Route component={createComponent(NotFound)}/>
+    </Switch>
+  </div>
 );
 
 export default getRouter;
